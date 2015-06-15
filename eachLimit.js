d78aed7cbb8d79f71f060de0ff5919ce9e93281c
ritesh7792 <ritesh.7792@gmail.com>
2015-06-15 16:44:19 +0530

    each limit function

--- eachLimit.js
+++ eachLimit.js
@@ -1,35 +1,23 @@
 /**
  * Created by ritesh on 6/15/15.
  */
-
-
-var arr = [1,2,3,4,5,6,7,8,9];
-
-function iterator(a){
-    console.log(a+5);
-}
-
-
 function eachLimit(arr,limit,iterator,callback){
     var curr = 0;
     var lastProcessedTill = 0;
     var i = 0;
     while(i < arr.length){
-        debugger;
         console.log("current position: " +i);
         if(curr < limit){
             i++;
-            debugger;
             console.log("curr < limit: " +curr + "i value: " +i);
             curr++;
             lastProcessedTill = i;
-            iterator(arr[i],function(){
-                    curr--;
+            iterator(arr[i],function(err,data){
+                if (err) callback(err);
+                curr--;
             })
         }
     }
 }
 
-eachLimit(arr,3,iterator,function(err){
-    if (err) throw err;
-})
+module.exports.eachLimit = eachLimit;
